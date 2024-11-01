ff7b7b29f527a01115cb32f5cfa2430f6c3086fb
mingli <mingli.yuan@gmail.com>
2024-11-01 12:52:59 +0800

    bump to 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wxbtool",
-    version="0.1.4",
+    version="0.1.5",
     author="Mingli Yuan",
     author_email="mingli.yuan@gmail.com",
     description="A toolkit for WeatherBench based on PyTorch",
